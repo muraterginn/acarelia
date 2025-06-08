426ca91e2ff560ca1908bdb229515d7e91b804cd
root <root@debian12.keyubu.com>
2025-06-08 03:13:37 +0300

    frontend service containarized

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['acarelia.com', '.acarelia.com'],
     proxy: {
       '/api': {
         target: 'http://31.58.91.32:8000',
